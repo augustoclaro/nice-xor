e0a992f25810a0fedf996480b75ee57d51013bb0
Augusto Claro <augustoclaro1@hotmail.com>
2015-07-25 11:21:28 -0300

    fix

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ var hex = require('./modules/hex.js');
 hex.bin = bin;
 bin.hex = hex;
 
+var doXor = function(binStr, binKey){
+  var keyIndex = 0;
+  return binStr.split('').map(function(item){
+    if (keyIndex >= binKey.length) keyIndex = 0;
+    return parseInt(item,10) ^ parseInt(binKey[keyIndex++], 10);
+  }).join('');
+};
+
 var xor = {
   crypt: function(str, key){
-    return bin.toHex(this.doXor(bin.toBin(str), bin.toBin(key)));
+    return bin.toHex(doXor(bin.toBin(str), bin.toBin(key)));
   },
   decrypt: function(str, key){
-    return bin.toStr(this.doXor(hex.toBin(str), bin.toBin(key)));
-  },
-  doXor: function(binStr, binKey){
-    var keyIndex = 0;
-    return binStr.split('').map(function(item){
-      if (keyIndex >= binKey.length) keyIndex = 0;
-      return parseInt(item,10) ^ parseInt(binKey[keyIndex++], 10);
-    }).join('');
+    return bin.toStr(doXor(hex.toBin(str), bin.toBin(key)));
   }
 };
 
